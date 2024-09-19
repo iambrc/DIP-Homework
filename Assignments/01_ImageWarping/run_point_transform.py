8e4bdd783f15ba76b17ee47c23e18e5d6972f90a
iambrc <iambrc@mail.ustc.edu.cn>
2024-09-19 21:45:05 +0800

    finish all deformation

--- Assignments/01_ImageWarping/run_point_transform.py
+++ Assignments/01_ImageWarping/run_point_transform.py
@@ -82,9 +82,54 @@ def MLS_affine_deformation(img, source_pts, target_pts, alpha=1.0, eps=1e-8):
     return warped_image
 
 
-# 执行RBF变换
-def RBF_deformation(image, source_pts, target_pts, alpha=1.0, eps=1e-8):
-    warped_image = np.array(image)
+# 执行RBF变换，RBF函数：(r^2+d^2)^alpha
+def RBF_deformation(img, source_pts, target_pts, alpha=0.5, r=10.0):
+    # 考虑反向映射才能用最后的remap函数，否则最后结果是反过来的
+    tmp = source_pts
+    source_pts = target_pts
+    target_pts = tmp
+
+    height, width = img.shape[:2]
+    N = source_pts.shape[0]
+    # 构建方程组Mx=y
+    y = np.append(target_pts.T.reshape(2 * N), [0, 0, 0, 0, 0, 0]).T
+    Px = source_pts[:, 0].reshape(N, 1).repeat(N, axis=1)
+    Py = source_pts[:, 1].reshape(N, 1).repeat(N, axis=1)
+    Pxy = np.power(np.power(np.subtract(Px, Px.T), 2) + np.power(np.subtract(Py, Py.T), 2) + r ** 2, alpha)
+    # 添加约束并构建M
+    M = np.zeros([2 * N + 6, 2 * N + 6])
+    M[0:N, 0:N] = Pxy
+    M[N:2 * N, N:2 * N] = Pxy
+    M[0:N, 2 * N:2 * N + 2] = source_pts
+    M[N:2 * N, 2 * N + 2:2 * N + 4] = source_pts
+    M[2 * N:2 * N + 2,0:N] = source_pts.T
+    M[2 * N + 2:2 * N + 4, N:2 * N] = source_pts.T
+    M[0:N, 2 * N + 4:2 * N + 5] = np.ones([N, 1])
+    M[N:2 * N, 2 * N + 5:2 * N + 6] = np.ones([N, 1])
+    M[2 * N + 4:2 * N + 5, 0:N] = np.ones([1, N])
+    M[2 * N + 5:2 * N + 6, N:2 * N] = np.ones([1, N])
+    # 得到所有的系数
+    x = np.linalg.solve(M, y)
+    # 计算变形后坐标fv
+    pcth = np.repeat(np.arange(height).reshape(height, 1), [width], axis=1)
+    pctw = np.repeat(np.arange(width).reshape(width, 1), [height], axis=1).T
+    img_coordinate = np.swapaxes(np.array([pctw, pcth]), 1, 2).T
+    fv = np.zeros([height, width, 2])
+    # height * width * N * 1
+    RBFvalue = np.power(np.linalg.norm(np.subtract(source_pts, img_coordinate.reshape(height, width, 1, 2)), axis=3), 2)
+    RBFvalue = np.power(RBFvalue + r ** 2, alpha).reshape(height, width, N, 1)
+    coffx = np.repeat(x[0:N].reshape(1, N), width, axis=0)
+    coffx = np.repeat(coffx.reshape(1, width, N), height, axis=0).reshape(height, width, 1, N)
+    coffy = np.repeat(x[N:2 * N].reshape(1, N), width, axis=0)
+    coffy = np.repeat(coffy.reshape(1, width, N), height, axis=0).reshape(height, width, 1, N)
+    fv[:, :, 0] = (np.matmul(coffx, RBFvalue).reshape(height, width) + x[2 * N] * img_coordinate[:, :, 0] +
+                   x[2 * N + 1] * img_coordinate[:, :, 1] + x[2 * N + 4])
+    fv[:, :, 1] = (np.matmul(coffy, RBFvalue).reshape(height, width) + x[2 * N + 2] * img_coordinate[:, :, 0] +
+                   x[2 * N + 3] * img_coordinate[:, :, 1] + x[2 * N + 5])
+    # 利用opencv remap函数进行重映射
+    mapxy = np.float32(fv)
+    warped_image = cv2.remap(img, mapxy[:, :, 0], mapxy[:, :, 1], borderMode=cv2.BORDER_WRAP,
+                             interpolation=cv2.INTER_LINEAR)
     return warped_image
 
 
@@ -162,10 +207,7 @@ def MLS_rigid_deformation(img, source_pts, target_pts, alpha=1.0, eps=1e-8):
     q_hat = np.subtract(target_pts, q_star.reshape(height, width, 1, 2))
     # 计算miu_s : height * width * 1
     pihat = p_hat.reshape(height, width, N, 1, 2)
-    pihatT = np.swapaxes(pihat, 3, 4)
     qihat = q_hat.reshape(height, width, N, 1, 2)
-    miu_s = np.sum(wi.reshape(height, width, N, 1) * np.matmul(pihat, pihatT).reshape(height, width, N, 1),
-                   axis=2).reshape(height, width, 1)
     # 计算矩阵Ai : height * width * N * 2 * 2
     l1 = pihat
     l2 = np.concatenate((l1[:, :, :, :, 1], -l1[:, :, :, :, 0]), axis=3).reshape(height, width, N, 1, 2)
@@ -188,8 +230,42 @@ def MLS_rigid_deformation(img, source_pts, target_pts, alpha=1.0, eps=1e-8):
 
 
 # 执行IDW变换
-def IDW_deformation(image, source_pts, target_pts, alpha=1.0, eps=1e-8):
-    warped_image = np.array(image)
+def IDW_deformation(img, source_pts, target_pts, alpha=2.0, eps=1e-8):
+    # 考虑反向映射才能用最后的remap函数，否则最后结果是反过来的
+    tmp = source_pts
+    source_pts = target_pts
+    target_pts = tmp
+
+    height, width = img.shape[:2]
+    N = source_pts.shape[0]
+    pcth = np.repeat(np.arange(height).reshape(height, 1), [width], axis=1)
+    pctw = np.repeat(np.arange(width).reshape(width, 1), [height], axis=1).T
+    img_coordinate = np.swapaxes(np.array([pctw, pcth]), 1, 2).T
+    # 计算变换的矩阵Di = Ai^(-1) * Bi以及权重Wi
+    Pij = np.repeat(source_pts.reshape(N, 1, 2), [N], axis=1).reshape(N, N, 2, 1)
+    Pij = np.subtract(Pij, np.swapaxes(Pij, 0, 1))
+    Qij = np.repeat(target_pts.reshape(N, 1, 2), [N], axis=1).reshape(N, N, 2, 1)
+    Qij = np.subtract(Qij, np.swapaxes(Qij, 0, 1))
+    Sigma_ij = np.reciprocal(np.power(np.linalg.norm(Pij, axis=2), alpha) + eps).reshape(N, N)
+    np.fill_diagonal(Sigma_ij, 0.0)
+    Sigma_ij = np.repeat(Sigma_ij.reshape(N, N, 1, 1), [4], axis=2).reshape(N, N, 2, 2)
+    Ai = np.sum(Sigma_ij * np.matmul(Pij, np.swapaxes(Pij, 2, 3)), axis=1).reshape(N, 2, 2)
+    Bi = np.sum(Sigma_ij * np.matmul(Qij, np.swapaxes(Pij, 2, 3)), axis=1).reshape(N, 2, 2)
+    Di = np.matmul(np.linalg.inv(Ai), Bi)
+
+    Sigma_i = np.reciprocal(np.power(
+        np.linalg.norm(np.subtract(source_pts, img_coordinate.reshape(height, width, 1, 2)), axis=3), alpha))
+    Wi = np.divide(Sigma_i, np.sum(Sigma_i, axis=2).reshape(height, width, 1)).reshape(height, width, N, 1, 1)
+    # 计算相似变形后的坐标fv
+    qi = np.repeat(target_pts.reshape(1, 1, N, 2, 1), [height], axis=0)
+    qi = np.repeat(qi, [width], axis=1)
+    fv_ = np.matmul(Di, np.subtract(img_coordinate.reshape(height, width, 1, 2),
+                                    source_pts).reshape(height, width, N, 2, 1)) + qi
+    fv = np.sum(np.matmul(fv_, Wi), axis=2).reshape(height, width, 2)
+    # 利用opencv remap函数进行重映射
+    mapxy = np.float32(fv)
+    warped_image = cv2.remap(img, mapxy[:, :, 0], mapxy[:, :, 1], borderMode=cv2.BORDER_WRAP,
+                             interpolation=cv2.INTER_LINEAR)
     return warped_image
 
 
